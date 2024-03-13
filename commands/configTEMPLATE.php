d37cfd29510f1a4bc9afa5591a8bfd9334ae9a4b
Robert Yacovelli <ryacovel@student.ubc.ca>
2024-03-12 17:04:15 -0700

    Add config template file

---
+++ commands/configTEMPLATE.php
@@ -0,0 +1,15 @@
+<!-- Rename this as config.php in your own environment this is JUST A TEMPLATE -->
+<?php
+return [
+    'database' => [
+        'host' => 'localhost',
+        'user' => '',
+        'password' => '',
+        'name' => 'social',
+    ],
+    'server' => [
+        'host' => 'localhost',
+        'port' => 8080,
+    ],
+];
+?>
