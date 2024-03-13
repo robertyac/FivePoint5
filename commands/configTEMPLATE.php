75e3a52857d566a876916512921604d1dc494562
Robert Yacovelli <ryacovel@student.ubc.ca>
2024-03-12 19:14:35 -0700

    Update database credentials in configTEMPLATE.php

--- commands/configTEMPLATE.php
+++ commands/configTEMPLATE.php
@@ -1,12 +1,11 @@
 <!-- Rename this as config.php in your own environment this is JUST A TEMPLATE -->
-<!-- THIS IS INTEGRATED WITH GITHUB ACTIONS TO AUTO FILL SECRETS -->
 <?php
 return [
     'database' => [
         'host' => 'localhost',
-        'user' => getenv("USER"),
-        'password' => getenv("PASSWORD"),
-        'name' => getenv("DB"),
+        'user' => '',
+        'password' => '',
+        'name' => '',
     ],
     'server' => [
         'host' => 'localhost',
