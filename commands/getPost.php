f9aa0f193dc050962eab9f8b4fecaca08efe8fe6
Robert Yacovelli <ryacovel@student.ubc.ca>
2024-03-13 16:44:12 -0700

    Refactor PDO connection and remove unnecessary options

--- commands/getPost.php
+++ commands/getPost.php
@@ -8,14 +8,9 @@ $pass = $config['database']['password'];
 $charset = 'utf8mb4';
 
 $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
-$opt = [
-    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
-    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-    PDO::ATTR_EMULATE_PREPARES   => false,
-];
 
 try {
-    $pdo = new PDO($dsn, $user, $pass, $opt);
+    $pdo = new PDO($dsn, $user, $pass);
 
     $sql = "SELECT Post.PostID, Post.PostTitle, Post.PostImage, Post.Description, Post.PostDateTime, Post.Rating, GROUP_CONCAT(Tag.Name) AS Tags
             FROM Post
@@ -30,7 +25,7 @@ try {
         echo "No posts found";
     }
 
-    // return the posts array to be used in index.php 
+    // return the posts array used in index.php 
     return $posts;
 } catch (PDOException $e) {
     die("PDO error: " . $e->getMessage());
