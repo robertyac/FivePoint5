35bf5c0dca98a6c13be902ba84e716762291d03a
Robert Yacovelli <ryacovel@student.ubc.ca>
2024-03-19 13:41:58 -0700

    Update icon and touch icon paths

--- index.php
+++ index.php
@@ -5,8 +5,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>FivePoint5 - Home</title>
-    <link rel="icon" type="image/x-icon" href="/img/5.5.ico">
-    <link rel="apple-touch-icon" href="/img/5.5-white.png">
+    <link rel="icon" type="image/x-icon" href="img/5.5.ico">
+    <link rel="apple-touch-icon" href="img/5.5-white.png">
     <!-- Index stylesheet -->
     <link rel="stylesheet" href="css/index.css">
     <!-- bootstrap -->
