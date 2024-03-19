5d9020f6bf4f6fe39c3486f0625bf7a2ad11cf7c
Connor <connor.cahoon@gmail.com>
2024-03-18 20:54:40 -0700

    Finished Login logic, validation and error handling

--- commands/register.php
+++ commands/register.php
@@ -15,29 +15,70 @@ if ($mysqli->connect_error) {
 echo var_dump($_POST);
 echo "<br>";
 echo var_dump($_FILES);
+$username = $_POST['usernameInput'];
+$email = $_POST['emailInput'];
 $password = $_POST['passwordInput'];
 $hash = hash('sha256', $password);
 
-if (isset($_FILES['profilePicInput']) && $_FILES['profilePicInput']['error'] === UPLOAD_ERR_OK) {
-    $imagePath = $_FILES['profilePicInput']['tmp_name'];
-    echo 'pic found ' . $imagePath;
+$check = testIfUsernameOrEmailIsTaken($username, $email);
+if ($check != "") {
+    echo '<script>console.log("' . $check . '")</script>';
+    echo $check;
+    session_start();
+    $_SESSION['alert'] = $check;
+    header('Location: ../index.php');
 } else {
-    echo "<script>console.log('no pic found');</script>";
-    $imagePath = "../img/defaultProfilePic.png";
-}
-$profilePicture = file_get_contents($imagePath);
+    if (isset($_FILES['profilePicInput']) && $_FILES['profilePicInput']['error'] === UPLOAD_ERR_OK) {
+        $imagePath = $_FILES['profilePicInput']['tmp_name'];
+        echo 'pic found ' . $imagePath;
+    } else {
+        echo "<script>console.log('no pic found');</script>";
+        $imagePath = "../img/defaultProfilePic.png";
+    }
+    $profilePicture = file_get_contents($imagePath);
 
-$sql = "INSERT INTO User (Username, Email, PasswordHash, ProfilePicture)
+    $sql = "INSERT INTO User (Username, Email, PasswordHash, ProfilePicture)
                 VALUES (?, ?, ?, ?)";
-$stmt = $mysqli->prepare($sql);
-$stmt->bind_param('ssss', $_POST['usernameInput'], $_POST['emailInput'], $hash, $profilePicture);
-$stmt->execute();
+    $stmt = $mysqli->prepare($sql);
+    $stmt->bind_param('ssss', $username, $email, $hash, $profilePicture);
+    $stmt->execute();
 
-
-echo "User added successfully";
+    echo "User added successfully";
 // close the connection
-$mysqli->close();
-session_start();
-$_SESSION['user'] = $_POST['usernameInput'];
+    $mysqli->close();
+    session_start();
+    $_SESSION['user'] = $_POST['usernameInput'];
 // redirect to index.php
-header('Location: ../index.php');
+    header('Location: ../index.php');
+}
+
+
+function testIfUsernameOrEmailIsTaken($username, $email) {
+    try {
+        $config = require 'config.php';
+        $host = $config['database']['host'];
+        $db = $config['database']['name'];
+        $user = $config['database']['user'];
+        $pass = $config['database']['password'];
+        $charset = 'utf8mb4';
+        $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+        $pdo = new PDO($dsn, $user, $pass);
+
+        // SQL query to get the posts with the given search term and tag
+        $sql = "SELECT Username, Email FROM User WHERE Username = :username OR Email = :email";
+
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute(['username' => $username, 'email' => $email]);
+        $results = $stmt->fetchAll();
+        if ($results) {
+            if ($results[0]['Username'] == $username) {
+                return "Username already in use. Try logging in";
+            } else {
+                return "Email already taken. Try logging in";
+            }
+        }
+        return "";
+    } catch (PDOException $e) {
+        die("PDO error: " . $e->getMessage());
+    }
+}
