5bc5d8f35243c5d50d7f6a32ccf7775829c0f420
Robert Yacovelli <ryacovel@student.ubc.ca>
2024-03-19 21:50:44 -0700

    Update Bootstrap and jQuery versions

--- commands/getPost.php
+++ commands/getPost.php
@@ -52,8 +52,10 @@ try {
             </div>
         </div>
 
-        <script src='https://code.jquery.com/jquery-3.3.1.slim.min.js'></script>
-        <script src='https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js'></script>
+        <!-- Bootstrap -->
+        <script src='https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js' integrity='sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL' crossorigin='anonymous'></script>
+        <!-- jquery -->
+        <script src='https://code.jquery.com/jquery-3.7.1.min.js'></script>
 
         <script type='text/javascript'>
             function redirect() {
