a8d899d317e18ff59f5609fb700fe5edf4218583
Davis Franklin <davis_franklin@outlook.com>
2024-03-20 19:37:08 -0700

    Fixed links and got the comments working

--- commands/submitComment.php
+++ commands/submitComment.php
@@ -1,7 +1,7 @@
 <?php
 session_start(); // Start the session
 
-$config = require '../commands/config.php';
+$config = require 'config.php';
 $host = $config['database']['host'];
 $db = $config['database']['name'];
 $user = $config['database']['user'];
@@ -14,7 +14,7 @@ try {
 
     if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         $postID = $_SESSION['postID']; // Get the postID from the session
-        $userID = $_SESSION['userID']; // Get the userID from the session
+        $userID = $_SESSION['user_id']; // Get the userID from the session
         $content = $_POST['content'];
 
         $sql = "INSERT INTO Comment (PostID, UserID, Content) VALUES (?, ?, ?)";
@@ -22,7 +22,7 @@ try {
         $stmt->execute([$postID, $userID, $content]);
 
         // Redirect back to the post page after successfully adding the comment
-        header("Location: viewPost.php?PostID=$postID#commentForm");
+        header("Location: ../viewPost.php?PostID=$postID#commentForm");
         exit;
     }
 } catch (PDOException $e) {
