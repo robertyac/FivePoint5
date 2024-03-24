59f15296c738702651302513a6d553f5f12f9845
Davis Franklin <davis_franklin@outlook.com>
2024-03-24 10:07:20 -0700

    removed console errors

--- createPost.php
+++ createPost.php
@@ -4,15 +4,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="reset.css" />
-    <link rel="stylesheet" href="post.css" />
     <title>Create a Post</title>
     <link rel="icon" type="image/x-icon" href="/img/5.5.ico">
     <link rel="apple-touch-icon" href="/img/5.5-white.png">
     <!-- bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <link rel="stylesheet" href="/post/createPost.css">
     <!-- CSS -->
     <link rel="stylesheet" type="text/css" href="css/createPost.css">
 </head>
@@ -77,11 +74,6 @@
         integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
         crossorigin="anonymous"></script>
     <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
-    <script>
-        $(function () {
-            $("#nav").load("/nav.html");
-        });
-    </script>
     <script>
         // Function to display the selected image
         function displayImage(files) {
--- viewPost.php
+++ viewPost.php
@@ -42,8 +42,6 @@ if (!$averageRating) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="reset.css" />
-    <link rel="stylesheet" href="post.css" />
     <title>View Post</title>
     <!-- bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
@@ -170,11 +168,6 @@ if (!$averageRating) {
     <!-- Scripts -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
     <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
-    <script>
-        $(function() {
-            $("#nav").load("../display_elements/nav.php");
-        });
-    </script>
     <script>
         // Update the rating display when the slider is moved
         function updateRatingDisplay(value) {
