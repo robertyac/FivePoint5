da4bc6af923ffd5d25ba5072262483f26d34c499
Robert Yacovelli <ryacovel@student.ubc.ca>
2024-04-08 00:01:31 -0700

    Add Admin page and functionality

--- commands/submitComment.php
+++ commands/submitComment.php
@@ -1,5 +1,6 @@
 <?php
 session_start(); // Start the session
+include 'checkUserStatus.php'; // Include the checkUserStatus.php file
 
 $config = require 'config.php';
 $host = $config['database']['host'];
@@ -13,24 +14,25 @@ try {
     $pdo = new PDO($dsn, $user, $pass);
 
     if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-        $postID = $_SESSION['postID'] ?? null; // Get the postID from the session
-        $userID = $_SESSION['user_id'] ?? null; // Get the userID from the session
+        $postID = $_SESSION['postID'] ?? null;
+        $userID = $_SESSION['user_id'] ?? null;
 
         // Check if the user is logged in
         if ($userID === null) {
-            // Redirect to viewPost.php with error message
             header("Location: ../viewPost.php?PostID=$postID&error=You must be logged in to comment.#commentForm");
             exit();
         }
 
+        // Check if the user is enabled
+        checkUserStatus($userID);
+
         $content = $_POST['content'];
 
-    // Check if comment is provided and its length is between 1 and 1000 characters
-    if ($content === null || trim($content) === '' || strlen($content) < 1 || strlen($content) > 1000) {
-        // Redirect to viewPost.php with error message
-        header("Location: ../viewPost.php?PostID=$postID&error=Comment is required and must be between 1 and 1000 characters.#commentForm");
-        exit();
-    }
+        // Check if comment is provided and its length is between 1 and 1000 characters
+        if ($content === null || trim($content) === '' || strlen($content) < 1 || strlen($content) > 1000) {
+            header("Location: ../viewPost.php?PostID=$postID&error=Comment is required and must be between 1 and 1000 characters.#commentForm");
+            exit();
+        }
 
         $sql = "INSERT INTO Comment (PostID, UserID, Content) VALUES (?, ?, ?)";
         $stmt = $pdo->prepare($sql);
